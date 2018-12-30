6eeef10666c75199383f9d2c55ad2622dffb3314
chenchaocom <1845360542@qq.com>
2018-12-30 13:58:23 +0800

    first commit

---
+++ src/main/java/com/crazyit/cloud/DemoTewts/DemoTewtsApplication.java
@@ -0,0 +1,14 @@
+package com.crazyit.cloud.DemoTewts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTewtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTewtsApplication.class, args);
+	}
+
+}
+
